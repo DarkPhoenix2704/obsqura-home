d01248fc9658bde9004475c518aeb57198f03532
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-04-16 07:17:30 +0530

    some more

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,25 +1,62 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { FaBars, FaHamburger } from "react-icons/fa";
 
 const NavBar = () => {
+  const router = useRouter();
   return (
     <div className="flex z-[10] flex-row items-center justify-between">
       <h1 className="text-white-100 text-sm">
         National Techo Cultural Fiesta of ICET
       </h1>
-      <FaBars className="text-white-1000 z-[2000] block rounded-md md:hidden bg-shark-950 p-1 w-8 h-8" />
+      <FaBars className="text-white-1000 focus: transform rotate-90 z-[2000] block rounded-md md:hidden bg-shark-950 p-1 w-8 h-8" />
       <div className="flex-row gap-4 px-8 rounded-lg hidden md:flex bg-shark-950">
-        <p className="p-2 text-lg font-semibold text-white-1000 transition-all cursor-pointer duration-100 delay-75 ease-linear hover:text-green-400">
-          Home
-        </p>
-        <p className="p-2 text-lg font-semibold text-white-1000 transition-all cursor-pointer duration-100 delay-75 ease-linear hover:text-blue-ribbon-400">
-          Events
-        </p>
-        <p className="p-2 text-lg font-semibold text-white-1000 transition-all cursor-pointer duration-100 delay-75 ease-linear  hover:text-red-500">
-          Workshops
-        </p>
-        <p className="p-2 text-lg font-semibold text-white-1000 transition-all cursor-pointer duration-100 delay-75 ease-linear  hover:text-california-500">
-          Competitions
-        </p>
+        <Link
+          href="/"
+          className={
+            router.pathname === "/" ? "text-green-400" : " text-white-1000"
+          }
+        >
+          <p className="p-2 text-lg font-semibold  transition-all cursor-pointer duration-100 delay-75 ease-linear hover:text-green-400">
+            Home
+          </p>
+        </Link>
+        <Link
+          href="/events"
+          className={
+            router.pathname === "/events?type=event"
+              ? "text-blue-ribbon-400"
+              : "text-white-1000"
+          }
+        >
+          <p className="p-2 text-lg font-semibold transition-all cursor-pointer duration-100 delay-75 ease-linear hover:text-blue-ribbon-400">
+            Events
+          </p>
+        </Link>
+        <Link
+          href="/events"
+          className={
+            router.pathname === "/events?type=workshop"
+              ? "text-red-500"
+              : "text-white-1000"
+          }
+        >
+          <p className="p-2 text-lg font-semibold transition-all cursor-pointer duration-100 delay-75 ease-linear  hover:text-red-500">
+            Workshops
+          </p>
+        </Link>
+        <Link
+          href="/events"
+          className={
+            router.pathname === "/events?type=workshop"
+              ? "text-california-500"
+              : "text-white-1000"
+          }
+        >
+          <p className="p-2 text-lg font-semibold 0 transition-all cursor-pointer duration-100 delay-75 ease-linear  hover:text-california-500">
+            Competitions
+          </p>
+        </Link>
       </div>
     </div>
   );
---
+++ pages/events.js
@@ -0,0 +1,10 @@
+const Events = () => {
+  return (
+    <div>
+      <h1>Events Page</h1>
+      <p>Events Page Content</p>
+    </div>
+  );
+};
+
+export default Events;
