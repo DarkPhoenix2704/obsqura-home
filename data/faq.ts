c6b1e51e87e8a6df13e72fbd5612baf30374bf90
Anbarasu <anbarasun123@gmail.com>
2023-04-25 19:34:09 -0700

    Update faq.ts

--- data/faq.ts
+++ data/faq.ts
@@ -5,7 +5,7 @@ const faqs = [
   },
   {
     question: "When is Obsqura?",
-    answer: "Mark your calendars and get ready to experience a dazzling display of technology and innovation, as the much-awaited Obsqura Technical Fest is set to enthral you on the exquisite date of May 3,4 and 5",
+    answer: "Mark your calendars and get ready to experience a dazzling display of technology and innovation, as the much-awaited Obsqura Technical Fest is set to enthral you on the exquisite date of May 4,5 and 6",
   },
 ];
 
